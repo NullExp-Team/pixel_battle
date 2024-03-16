c87e0542c063e40b6fac4bff7f3be763734d8fee
StarProxima <maks.prozorov1@yandex.ru>
2024-03-17 01:11:43 +0300

    FieldImageService

--- shared/lib/src/features/field/domain/field_image_service.g.dart
+++ shared/lib/src/features/field/domain/field_image_service.g.dart
@@ -1,22 +1,22 @@
 // GENERATED CODE - DO NOT MODIFY BY HAND
 
-part of 'field_state_service.dart';
+part of 'field_image_service.dart';
 
 // **************************************************************************
 // RiverpodGenerator
 // **************************************************************************
 
-String _$fieldStateServiceHash() => r'6265c60da837c07172d2556070be8b889db660b5';
+String _$fieldImageServiceHash() => r'311e4eef1e58fc4290744c16952068d5740c0392';
 
-/// See also [FieldStateService].
-@ProviderFor(FieldStateService)
-final fieldStateServiceProvider = AutoDisposeStreamNotifierProvider<
-    FieldStateService, FieldStateMap>.internal(
-  FieldStateService.new,
-  name: r'fieldStateServiceProvider',
+/// See also [FieldImageService].
+@ProviderFor(FieldImageService)
+final fieldImageServiceProvider =
+    AutoDisposeAsyncNotifierProvider<FieldImageService, ui.Image>.internal(
+  FieldImageService.new,
+  name: r'fieldImageServiceProvider',
   debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
       ? null
-      : _$fieldStateServiceHash,
+      : _$fieldImageServiceHash,
   dependencies: <ProviderOrFamily>[webSocketApiProvider],
   allTransitiveDependencies: <ProviderOrFamily>{
     webSocketApiProvider,
@@ -24,6 +24,6 @@ final fieldStateServiceProvider = AutoDisposeStreamNotifierProvider<
   },
 );
 
-typedef _$FieldStateService = AutoDisposeStreamNotifier<FieldStateMap>;
+typedef _$FieldImageService = AutoDisposeAsyncNotifier<ui.Image>;
 // ignore_for_file: type=lint
 // ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
