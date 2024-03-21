b47e5cd0231dca49cdc926f16f8e11539bb9c4bb
delta-null <delta.null@vk.com>
2024-03-21 13:39:00 +0300

    online users list

--- apps/admin_app/lib/app/view/widgets/users_panel.dart
+++ apps/admin_app/lib/app/view/widgets/users_panel.dart
@@ -1,17 +1,39 @@
 import 'package:core/core.dart';
 import 'package:flutter/material.dart';
 
+import '../../controller/admin_controller.dart';
+
 class UsersPanel extends HookConsumerWidget {
   const UsersPanel({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return const Padding(
-      padding: EdgeInsets.all(16),
+    final state = ref.watch(adminControllerProvider);
+
+    return Padding(
+      padding: const EdgeInsets.all(16),
       child: Align(
         alignment: Alignment.topLeft,
-        child:
-            Column(crossAxisAlignment: CrossAxisAlignment.start, children: []),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const SText.title('Пользователи'),
+            const Gap(12),
+            Flexible(
+              child: GridView.builder(
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 1,
+                  mainAxisSpacing: 12,
+                  mainAxisExtent: 20,
+                ),
+                itemCount: state.users.length,
+                itemBuilder: (context, index) {
+                  return SText.text(state.users[index].nickname);
+                },
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
